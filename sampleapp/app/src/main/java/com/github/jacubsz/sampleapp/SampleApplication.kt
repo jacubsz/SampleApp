7ea33cf91bc5cc6994485bb3ad8fa362fe48872f
Jakub Szostakowski <jakub.szostakowski@bestmile.com>
2021-06-08 23:08:56 +0200

    Global RX error handler

--- sampleapp/app/src/main/java/com/github/jacubsz/sampleapp/SampleApplication.kt
+++ sampleapp/app/src/main/java/com/github/jacubsz/sampleapp/SampleApplication.kt
@@ -1,9 +1,11 @@
 package com.github.jacubsz.sampleapp
 
+import android.util.Log
 import com.github.jacubsz.sampleapp.dagger.AppModule
 import com.github.jacubsz.sampleapp.dagger.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 
 class SampleApplication : DaggerApplication() {
 
@@ -12,4 +14,15 @@ class SampleApplication : DaggerApplication() {
             .factory()
             .create(AppModule(applicationContext))
             .also { it.inject(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+        configureGlobalRxErrorHandler()
+    }
+
+    private fun configureGlobalRxErrorHandler() {
+        RxJavaPlugins.setErrorHandler {
+            Log.e("RX stream error occurred", it.localizedMessage, it)
+        }
+    }
 }
