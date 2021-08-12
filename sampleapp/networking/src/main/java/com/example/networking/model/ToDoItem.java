3747a543636a50a9cff6900cf9efd0f398fba442
Jakub Szostakowski <jacubsz@gmail.com>
2021-08-12 10:04:08 +0200

    Test coverage for AppNetworkingPlugin

--- sampleapp/networking/src/main/java/com/example/networking/model/ToDoItem.java
+++ sampleapp/networking/src/main/java/com/example/networking/model/ToDoItem.java
@@ -1,6 +1,8 @@
 package com.example.networking.model;
 
- public class ToDoItem {
+import java.util.Objects;
+
+public class ToDoItem {
 
     private Integer id;
     private String content;
@@ -36,5 +38,18 @@ package com.example.networking.model;
 
     public void setChecked(Boolean checked) {
         this.checked = checked;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ToDoItem toDoItem = (ToDoItem) o;
+        return Objects.equals(id, toDoItem.id) && Objects.equals(content, toDoItem.content) && Objects.equals(checked, toDoItem.checked);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, checked);
     }
 }
